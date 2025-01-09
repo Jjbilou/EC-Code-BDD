0b0b756d07e97c2ea02c13b8986341e9dcc44fed
Nicolas <nico.dobosz@gmail.com>
2025-01-09 16:16:30 +0100

    Story 1 finie - Inscription, Connexion et Déconnexion

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,11 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\BookReadRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class HomeController extends AbstractController
 {
@@ -33,20 +38,50 @@ class HomeController extends AbstractController
 
 
     #[Route('/login', name: 'auth.login')]
-    public function login(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // Render the 'hello.html.twig' template
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app.home');
+        }
+
+        // Get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // Last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render('auth/login.html.twig', [
-            'name' => 'Thibaud', // Pass data to the view
+            'last_username' => $lastUsername,
+            'error'         => $error,
         ]);
     }
 
+    #[Route('/logout', name: 'auth.logout')]
+    public function logout(): void {}
+
     #[Route('/register', name: 'auth.register')]
-    public function register(): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $form->get('plainPassword')->getData();
+
+            // encode the plain password
+            $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('app.home');
+        }
         // Render the 'hello.html.twig' template
         return $this->render('auth/register.html.twig', [
-            'name' => 'Thibaud', // Pass data to the view
+            'form' => $form, // Pass data to the view
         ]);
     }
 }
